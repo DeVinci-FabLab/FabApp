7e7efb6d4d1275ae8d14e6e207ba72b51b569bd2
BriocheAF <thomasderudder@sfr.fr>
2023-07-11 14:34:37 +0200

    update of sql with functions to create custom errors

--- Backend/database/setup.sql
+++ Backend/database/setup.sql
@@ -1,4 +1,4 @@
-
+DROP TABLE IF EXISTS users_connection_code;
 DROP TABLE IF EXISTS articles;
 DROP TABLE IF EXISTS users;
 
@@ -10,7 +10,7 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     validated BOOLEAN DEFAULT FALSE,
     info JSONB DEFAULT '{}'::JSONB,
-    CONSTRAINT unique_username UNIQUE (username),
+    CONSTRAINT valid_username UNIQUE (username), CHECK (NOT (username ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')),
     CONSTRAINT valid_email UNIQUE (email), CHECK (email ~* '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')
 );
 
@@ -24,19 +24,28 @@ CREATE TABLE articles (
     CONSTRAINT fk_articles_users FOREIGN KEY (published_by) REFERENCES users (id)
 );
 
+--create table for users connection code
+CREATE TABLE users_connection_code (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    code INT NOT NULL,
+    CONSTRAINT fk_users_connection_code_users FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
 --create rules for users
 CREATE OR REPLACE FUNCTION check_duplicates()
 RETURNS TRIGGER AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM users WHERE username = NEW.username) THEN
         RAISE EXCEPTION 'Duplicate username: %', NEW.username using errcode = 23505;
-    END IF;
 
-    IF EXISTS (SELECT 1 FROM users WHERE email = NEW.email) THEN
+    ELSEIF EXISTS (SELECT 1 FROM users WHERE email = NEW.email) THEN
         RAISE EXCEPTION 'Duplicate email: %', NEW.email using errcode = 23505;
-    END IF;
-  
-    IF NEW.email !~* '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$' THEN
+ 
+    ELSEIF NEW.username ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$' THEN
+        RAISE EXCEPTION 'Incorrect username format: %', NEW.username using errcode = 23514;
+
+    ELSEIF NEW.email !~* '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$' THEN
         RAISE EXCEPTION 'Incorrect email format: %', NEW.email using errcode = 23514;
     END IF;
     RETURN NEW;
@@ -47,3 +56,4 @@ CREATE TRIGGER duplicates_trigger
 BEFORE INSERT ON users
 FOR EACH ROW
 EXECUTE FUNCTION check_duplicates();
+
